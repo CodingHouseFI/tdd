a687d02064f859510e1456ad67b8870108371138
Javier Escobar <javierescobar101@gmail.com>
2015-06-11 15:18:48 -0700

    results

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var doubler = function(a, b) {
+  var first = a * 1;
+  var second = b * 1;
+  return b * 2 + a * 2;
 }
 
-// console.assert(doubler(4,2) === 12);
-// console.assert(doubler(1,3) === 8);
-// console.assert(doubler(-1,3) === 4);
+console.assert(doubler(4,2) === 12);
+console.assert(doubler(1,3) === 8);
+console.assert(doubler(-1,3) === 4);
